8f314fdc9f9065b47a1073d2d1726e070d2ba1d2
Norberto Boa <nboa.26@gmail.com>
2025-01-15 21:36:32 +0200

    ADD: Create slug class for question slug]

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,20 +1,24 @@
 import { randomUUID } from "node:crypto";
+import type { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
   title: string;
   content: string;
   authorId: string;
+  slug: Slug;
 }
 
 export class Question {
   public id: string;
   public title: string;
+  public slug: Slug;
   public content: string;
   public authorId: string;
 
   constructor(props: QuestionProps, id?: string) {
     this.id = id ?? randomUUID();
     this.title = props.title;
+    this.slug = props.slug;
     this.content = props.content;
     this.authorId = props.authorId;
   }
---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,28 @@
+export class Slug {
+  public value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  /**
+   * Receives a string and normalizes it as a slug
+   *
+   * Example: "A super nice title" => "a-super-nice-title"
+   *
+   * @param text {string}
+   */
+  static createFromText(text: string) {
+    const slugText = text
+      .normalize("NFKD")
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, "-") // Remove whitespaces
+      .replace(/[^\w-]+/g, "") // Get everything that isn't a word
+      .replace(/_/g, "-") // Get all underscore and change to hifen
+      .replace(/--+/g, "-") // Remove duplicates hifens
+      .replace(/-$/g, ""); // Remove every hifen at the end
+
+    return new Slug(slugText);
+  }
+}
