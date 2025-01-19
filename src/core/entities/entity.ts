6f1760a89171d17720fb51732e730c3e9f2d607b
Norberto Boa <nboa.26@gmail.com>
2025-01-20 01:09:27 +0200

    ADD: Create a value object class for IDs
    Created this value object so that if I need to change the type of the
    ID, I only change it on one class and be applied for all other classes

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,15 +1,16 @@
 import { randomUUID } from "node:crypto";
+import { UniqueEntityID } from "./unique-entity-id";
 
 export class Entity<Props> {
-  private _id: string;
+  private _id: UniqueEntityID;
   protected props: Props;
 
-  get id(): string {
+  get id() {
     return this._id;
   }
 
   protected constructor(props: Props, id?: string) {
-    this._id = id ?? randomUUID();
     this.props = props;
+    this._id = new UniqueEntityID(id);
   }
 }
