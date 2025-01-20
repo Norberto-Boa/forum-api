dbabf96f2044bff84afc6475b7794f5c736aae91
Norberto Boa <nboa.26@gmail.com>
2025-01-20 22:11:11 +0200

    Use faker to fake data for tests

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,18 +1,23 @@
+import { faker } from '@faker-js/faker';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import {
   Question,
   type QuestionProps,
 } from '@/domain/forum/enterprise/entities/question';
-import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug';
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: 'Example Question',
-    content: 'Example Content',
-    slug: Slug.create('example-questi'),
-    ...override,
-  });
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+): Question {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  );
 
   return question;
 }
