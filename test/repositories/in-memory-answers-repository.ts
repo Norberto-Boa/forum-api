51bdac9146e7e70330813731ea6939e0b8367bd7
Norberto Boa <nboa.26@gmail.com>
2025-01-22 00:43:11 +0200

    test: prepare environment for delete answer test service

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,10 +1,26 @@
-import type { AnswersRepository } from '@/domain/forum/application/repositories/answer-repository';
+import type { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository';
 import type { Answer } from '@/domain/forum/enterprise/entities/answer';
 
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = [];
 
+  async findById(id: string): Promise<Answer | null> {
+    const answer = await this.items.find((item) => item.id.toString() === id);
+
+    if (!answer) {
+      return null;
+    }
+
+    return answer;
+  }
+
   async create(answer: Answer): Promise<void> {
     this.items.push(answer);
+  }
+
+  async delete(answer: Answer): Promise<void> {
+    const answerIndex = this.items.findIndex((item) => item.id === answer.id);
+
+    this.items.splice(answerIndex, 1);
   }
 }
