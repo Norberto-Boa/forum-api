c6569a197b73b3cfe8dda40b0622a562fbe471f4
Norberto Boa <nboa.26@gmail.com>
2025-02-04 13:45:45 +0200

    test: Test the commentOnQuestion Service
    - test if comment is being created
    - Ensure comment is being assigned to the proper question

---
+++ src/domain/forum/application/services/comment-on-question.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository';
+import { CommentOnQuestionService } from './comment-on-question';
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository';
+import { makeQuestion } from 'test/factories/make-question';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository;
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: CommentOnQuestionService;
+
+describe('CommentOnQuestionService', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository();
+    sut = new CommentOnQuestionService(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    );
+  });
+
+  it('should be able to comment on Question', async () => {
+    inMemoryQuestionsRepository.create(
+      makeQuestion({}, new UniqueEntityID('question-1')),
+    );
+
+    const { questionComment } = await sut.execute({
+      authorId: '1',
+      questionId: 'question-1',
+      content: 'this is a comment',
+    });
+
+    expect(questionComment.content).toEqual('this is a comment');
+    expect(questionComment.questionId.toString()).toEqual('question-1');
+  });
+});
