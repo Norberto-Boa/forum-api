0a4b6d121d86116fea3a07218e921a226b9f5065
Norberto Boa <nboa.26@gmail.com>
2025-02-04 13:42:38 +0200

    ADD: create comment-question repository
    - Create method create for the repository

---
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -0,0 +1,5 @@
+import type { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment';
+
+export interface QuestionCommentsRepository {
+  create(question: QuestionComment): Promise<void>;
+}
