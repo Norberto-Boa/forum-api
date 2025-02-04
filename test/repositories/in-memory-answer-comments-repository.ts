9b6e2689ec38c12a975e52a046d29be427451194
Norberto Boa <nboa.26@gmail.com>
2025-02-04 22:55:55 +0200

    ADD: Create the fetchManyRecent Method

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import type { PaginationParams } from '@/core/repositories/pagination-params';
 import type { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository';
 import type { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment';
 
@@ -26,5 +27,14 @@ export class InMemoryAnswerCommentsRepository
     );
 
     this.items.splice(itemIndex, 1);
+  }
+
+  async fetchManyRecent(params: PaginationParams, answerId: string) {
+    const items = await this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((params.page - 1) * 20, params.page * 20);
+
+    return items;
   }
 }
