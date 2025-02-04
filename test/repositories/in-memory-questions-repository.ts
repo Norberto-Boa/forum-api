cb1acd335320b58cb5179b94285a6336d8df07f6
Norberto Boa <nboa.26@gmail.com>
2025-02-04 10:04:44 +0200

    ADD: create findManyRecent method for the questions repository
    - Implement the method on the in-Memory-Repository
    - Make sure that the questions are sorted by the recent date

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import type { PaginationParams } from '@/core/repositories/pagination-params';
 import type { QuestionRepository } from '@/domain/forum/application/repositories/questions-repository';
 import type { Question } from '@/domain/forum/enterprise/entities/question';
 
@@ -39,5 +40,13 @@ export class InMemoryQuestionsRepository implements QuestionRepository {
     const itemIndex = this.items.findIndex((item) => item.id === question.id);
 
     this.items.splice(itemIndex, 1);
+  }
+
+  async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20);
+
+    return questions;
   }
 }
