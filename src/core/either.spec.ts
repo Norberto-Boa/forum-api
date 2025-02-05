3400f8314072c82f44c8af8422b57281c5b00401
Norberto Boa <nboa.26@gmail.com>
2025-02-05 11:24:41 +0200

    test: test functional error handling functionalities

---
+++ src/core/either.spec.ts
@@ -0,0 +1,27 @@
+import { left, right, type Either } from './either';
+
+function doSomething(shouldSuccess: boolean): Either<string, number> {
+  if (shouldSuccess) {
+    return right(10);
+  } else {
+    return left('Failed');
+  }
+}
+
+test('succes result', () => {
+  const result = doSomething(true);
+
+  if (result.isRight()) {
+    console.log(result.value);
+  }
+
+  expect(result.isLeft()).toBe(false);
+  expect(result.isRight()).toBe(true);
+});
+
+test('succes result', () => {
+  const result = doSomething(false);
+
+  expect(result.isLeft()).toBe(true);
+  expect(result.isRight()).toBe(false);
+});
