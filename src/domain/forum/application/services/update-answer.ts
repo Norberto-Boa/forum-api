d0e97d6fac2dbf312903e90469c0efde9b51514a
Norberto Boa <nboa.26@gmail.com>
2025-02-11 14:52:01 +0200

    ADD: Create update answer service

--- src/domain/forum/application/services/update-answer.ts
+++ src/domain/forum/application/services/update-answer.ts
@@ -2,11 +2,16 @@ import { left, right, type Either } from '@/core/either';
 import type { AnswersRepository } from '../repositories/answers-repository';
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
 import { NotAllowedError } from './errors/not-allowed-error';
+import { AnswerAttachmentRepository } from '../repositories/answer-attachments-repository';
+import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list';
+import { AnswerAttachment } from '../../enterprise/entities/answer-attachment';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
 interface UpdateAnswerServiceRequest {
   authorId: string;
   answerId: string;
   content: string;
+  attachmentsIds: string[];
 }
 
 type UpdateAnswerServiceResponse = Either<
@@ -15,12 +20,16 @@ type UpdateAnswerServiceResponse = Either<
 >;
 
 export class UpdateAnswerService {
-  constructor(private answerRepository: AnswersRepository) {}
+  constructor(
+    private answerRepository: AnswersRepository,
+    private answerAttachmentsRepository: AnswerAttachmentRepository,
+  ) {}
 
   async execute({
     authorId,
     answerId,
     content,
+    attachmentsIds,
   }: UpdateAnswerServiceRequest): Promise<UpdateAnswerServiceResponse> {
     const answer = await this.answerRepository.findById(answerId.toString());
 
@@ -32,7 +41,25 @@ export class UpdateAnswerService {
       return left(new NotAllowedError());
     }
 
+    const currentAnswersAttachments =
+      await this.answerAttachmentsRepository.findManyByAnswerId(
+        answer.id.toString(),
+      );
+
+    const answerAttachmentList = new AnswerAttachmentList(
+      currentAnswersAttachments,
+    );
+
+    const answerAttachments = attachmentsIds.map((attachmentId) => {
+      return AnswerAttachment.create({
+        answerId: answer.id,
+        attachmentId: new UniqueEntityID(attachmentId),
+      });
+    });
+
+    answerAttachmentList.update(answerAttachments);
     answer.content = content;
+    answer.attachments = answerAttachmentList;
 
     await this.answerRepository.save(answer);
 
