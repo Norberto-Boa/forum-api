761a354899ebd279e0cc75bdbceaccf38e53ed99
Norberto Boa <nboa.26@gmail.com>
2025-02-11 12:18:52 +0200

    ADD: Implement the edition of attachments on update question

--- src/domain/forum/application/services/update-question.ts
+++ src/domain/forum/application/services/update-question.ts
@@ -2,12 +2,17 @@ import { left, right, type Either } from '@/core/either';
 import type { QuestionRepository } from '../repositories/questions-repository';
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
 import { NotAllowedError } from './errors/not-allowed-error';
+import { QuestionAttachmentRepository } from '../repositories/question-attachments-repository';
+import { QuestionAttachmentList } from '../../enterprise/entities/question-attachment-list';
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
 interface UpdateQuestionServiceRequest {
   authorId: string;
   questionId: string;
   title: string;
   content: string;
+  attachmentsIds: string[];
 }
 
 type UpdateQuestionServiceResponse = Either<
@@ -16,17 +21,19 @@ type UpdateQuestionServiceResponse = Either<
 >;
 
 export class UpdateQuestionService {
-  constructor(private questionRepository: QuestionRepository) {}
+  constructor(
+    private questionRepository: QuestionRepository,
+    private questionAttachmentRepository: QuestionAttachmentRepository,
+  ) {}
 
   async execute({
     authorId,
     questionId,
     title,
     content,
+    attachmentsIds,
   }: UpdateQuestionServiceRequest): Promise<UpdateQuestionServiceResponse> {
-    const question = await this.questionRepository.findById(
-      questionId.toString(),
-    );
+    const question = await this.questionRepository.findById(questionId);
 
     if (!question) {
       return left(new ResourceNotFoundError());
@@ -36,8 +43,24 @@ export class UpdateQuestionService {
       return left(new NotAllowedError());
     }
 
+    const currentQuestionAttachments =
+      await this.questionAttachmentRepository.findManyByQuestionId(questionId);
+    const questionAttachmentList = new QuestionAttachmentList(
+      currentQuestionAttachments,
+    );
+
+    const questionAttachments = attachmentsIds.map((attachmentId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        questionId: question.id,
+      });
+    });
+
+    questionAttachmentList.update(questionAttachments);
+
     question.title = title;
     question.content = content;
+    question.attachments = questionAttachmentList;
 
     await this.questionRepository.save(question);
 
